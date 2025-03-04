2838b019a78e72a166828e87090c3a96afec18ab
kimromana <kimromana@gmail.com>
2025-03-04 14:00:59 +0500

    Initial commit

---
+++ src/main/java/com/glp/exp/view/user/UserListView.java
@@ -0,0 +1,14 @@
+package com.glp.exp.view.user;
+
+import com.glp.exp.entity.User;
+import com.glp.exp.view.main.MainView;
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.view.*;
+
+@Route(value = "users", layout = MainView.class)
+@ViewController(id = "User.list")
+@ViewDescriptor(path = "user-list-view.xml")
+@LookupComponent("usersDataGrid")
+@DialogMode(width = "64em")
+public class UserListView extends StandardListView<User> {
+}
